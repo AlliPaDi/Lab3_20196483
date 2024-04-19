0769829d060fc32f5e9c3f693a667cc8a116f57b
Allison <a20196483@pucp.edu.pe>
2024-04-18 20:42:48 -0500

    inicio

---
+++ Lab3_20196483/src/main/java/com/example/lab3_20196483/Lab320196483Application.java
@@ -0,0 +1,13 @@
+package com.example.lab3_20196483;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab320196483Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab320196483Application.class, args);
+    }
+
+}
---
+++ Lab3_20196483/src/test/java/com/example/lab3_20196483/Lab320196483ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab3_20196483;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab320196483ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
