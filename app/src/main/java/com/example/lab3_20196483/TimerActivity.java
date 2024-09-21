759efb3c5bce712fa177c117d2affa160c53d52e
Allison <a20196483@pucp.edu.pe>
2024-09-21 15:41:22 -0500

    Timer: inciso 2

--- app/src/main/java/com/example/lab3_20196483/TimerActivity.java
+++ app/src/main/java/com/example/lab3_20196483/TimerActivity.java
@@ -1,7 +1,10 @@
 package com.example.lab3_20196483;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,6 +14,7 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.example.lab3_20196483.databinding.ActivityMainBinding;
 import com.example.lab3_20196483.databinding.ActivityTimerBinding;
+import com.google.android.material.appbar.MaterialToolbar;
 
 public class TimerActivity extends AppCompatActivity {
 
@@ -38,11 +42,45 @@ public class TimerActivity extends AppCompatActivity {
             binding.ivGenero.setImageResource(R.drawable.ic_man);
         }
 
+        // Configurar el toolbar
+        MaterialToolbar toolbar = binding.topAppBar;
+        setSupportActionBar((toolbar));
+
         EdgeToEdge.enable(this);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+
+    // Log out
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.top_app_bar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.power) {
+            logout();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void logout() {
+        // Eliminar datos de sesión
+        SharedPreferences preferences = getSharedPreferences("user_session", MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.clear();  // Limpiar todas las preferencias de sesión
+        editor.apply();  // Aplicar los cambios
+
+        // Redirigir al usuario a la pantalla de Login
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
