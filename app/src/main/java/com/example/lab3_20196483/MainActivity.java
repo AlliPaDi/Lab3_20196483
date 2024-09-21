364bdc7837fc77659fab30e1911bf5f148bd5bb2
Allison <a20196483@pucp.edu.pe>
2024-09-21 00:45:47 -0500

    Vista Timer

--- app/src/main/java/com/example/lab3_20196483/MainActivity.java
+++ app/src/main/java/com/example/lab3_20196483/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.lab3_20196483;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +10,33 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.lab3_20196483.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    // Inicializando ViewBinding
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        // Inicializando ViewBinding
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // Usando ViewBinding para el botón btnLogin
+        binding.btnLogin.setOnClickListener(view -> {
+            ingresarPomodoro(view);
         });
+
+        EdgeToEdge.enable(this);
     }
+
+    // Ingresar a Pomodoro
+    public void ingresarPomodoro(View view){
+        Intent intent = new Intent(this, TimerActivity.class);
+        startActivity(intent);
+    }
+
 }
